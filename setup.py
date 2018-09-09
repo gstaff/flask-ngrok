b90524d778ad18bbf32165d993f4f4c093656934
Grant Stafford <grant.d.stafford@gmail.com>
2018-09-09 14:43:52 -0500

    Adjust timer delays

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flask-ngrok",
-    version="0.0.22",
+    version="0.0.23",
     author="Grant Stafford",
     description="A simple way to demo Flask apps from your machine.",
     long_description=long_description,
